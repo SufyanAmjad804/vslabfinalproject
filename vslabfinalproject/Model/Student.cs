0401e4f8429d468f94b81b3c345ad911f8c62a2a
SufyanAmjad804 <247237@students.au.edu.pk>
2026-05-19 22:55:53 +0500

    COMMIT 49

---
+++ vslabfinalproject/Model/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentDashboard.Web.Models;
+
+public class Student : Person
+{
+    [Required, StringLength(30)]
+    public string StudentNumber { get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string ProgramName { get; set; } = string.Empty;
+
+    [Range(1, 12)]
+    public int Semester { get; set; } = 1;
+
+    [StringLength(30)]
+    public string Status { get; set; } = "Active";
+
+    public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+    public ICollection<AttendanceRecord> AttendanceRecords { get; set; } = new List<AttendanceRecord>();
+    public ICollection<GradeRecord> GradeRecords { get; set; } = new List<GradeRecord>();
+    public ICollection<FeeInvoice> FeeInvoices { get; set; } = new List<FeeInvoice>();
+    public ICollection<SupportTicket> SupportTickets { get; set; } = new List<SupportTicket>();
+    public ICollection<StudyTask> StudyTasks { get; set; } = new List<StudyTask>();
+
+    public override string GetFullName() => $"{FirstName} {LastName} ({StudentNumber})".Trim();
+}
