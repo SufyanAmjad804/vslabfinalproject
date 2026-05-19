2b7a7f62f350b874312af9219028d417be0595f9
SufyanAmjad804 <247237@students.au.edu.pk>
2026-05-19 22:40:21 +0500

    COMMIT 40

---
+++ vslabfinalproject/Model/AttendanceRecord.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using vslabfinalproject.Components.Pages;
+
+namespace StudentDashboard.Web.Models;
+
+public class AttendanceRecord : BaseEntity
+{
+    [Required]
+    public int StudentId { get; set; }
+    public Student? Student { get; set; }
+
+    [Required]
+    public int CourseId { get; set; }
+    public Course? Course { get; set; }
+
+    public DateTime AttendanceDate { get; set; } = DateTime.Today;
+
+    [Required, StringLength(20)]
+    public string Status { get; set; } = "Present";
+
+    [StringLength(250)]
+    public string Remarks { get; set; } = string.Empty;
+}
